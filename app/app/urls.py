f38dad165352c7853e7206daec789ea9d0a24818
bencarneiro <bencarneiro@gmail.com>
2024-01-12 21:14:32 -0600

    add an html template, plus configure serving statics

--- app/app/urls.py
+++ app/app/urls.py
@@ -15,7 +15,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from backend.views import home
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path("admin/", admin.site.urls),
+    path("", home, name="home"),
 ]
--- app/backend/views.py
+++ app/backend/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+def home(request):
+    # latest_question_list = Question.objects.order_by("-pub_date")[:5]
+    context = {"latest_question_list": "hi"}
+    return render(request, "home.html", context)
