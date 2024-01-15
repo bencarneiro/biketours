ed1a59ef725d174740d861bc456be7fb74a3a41b
bencarneiro <bencarneiro@gmail.com>
2024-01-15 09:02:30 -0600

    register admin models

--- app/backend/admin.py
+++ app/backend/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
-
+from .models import Customer, Tour, TourType, TourGroup, TourSpot, TourRoute, GroupMember, CheckoutSession, CustomerWaiver 
 # Register your models here.
+admin.site.register(Customer)
+admin.site.register(Tour)
+admin.site.register(TourType)
+admin.site.register(TourGroup)
+admin.site.register(TourSpot)
+admin.site.register(TourRoute)
+admin.site.register(GroupMember)
+admin.site.register(CheckoutSession)
+admin.site.register(CustomerWaiver)
