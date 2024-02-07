db1f4f3a5627ba7c3848a76cff05cb903cd3b43d
bencarneiro <bencarneiro@gmail.com>
2024-02-07 15:35:45 -0600

    add tweeting to management command

---
+++ app/backend/management/commands/post_media.py
@@ -0,0 +1,79 @@
+from django.core.management.base import BaseCommand
+import tweepy
+import os
+
+def get_twitter_conn_v1(api_key, api_secret, access_token, access_token_secret) -> tweepy.API:
+    """Get twitter conn 1.1"""
+
+    auth = tweepy.OAuth1UserHandler(api_key, api_secret)
+    auth.set_access_token(
+        access_token,
+        access_token_secret,
+    )
+    return tweepy.API(auth)
+
+def get_twitter_conn_v2(api_key, api_secret, access_token, access_token_secret) -> tweepy.Client:
+    """Get twitter conn 2.0"""
+
+    client = tweepy.Client(
+        consumer_key=api_key,
+        consumer_secret=api_secret,
+        access_token=access_token,
+        access_token_secret=access_token_secret,
+    )
+
+    return client
+
+def tweet(tweet_text, media_filepath):
+    api_key = os.environ.get("TWITTER_CONSUMER_KEY", "")
+    api_secret = os.environ.get("TWITTER_CONSUMER_SECRET", "")
+    access_token = os.environ.get("TWITTER_ACCESS_TOKEN","")
+    access_token_secret = os.environ.get("TWITTER_ACCESS_TOKEN_SECRET","")
+    client_v1 = get_twitter_conn_v1(api_key, api_secret, access_token, access_token_secret)
+    client_v2 = get_twitter_conn_v2(api_key, api_secret, access_token, access_token_secret)
+    # media_path = "/home/tonydeals/app/biketours/app/backend/static/good_skyline_ben.jpg"
+    media_ids = []
+    try:
+        if media_filepath:
+            media = client_v1.media_upload(filename=media_filepath)
+            media_ids += [media.media_id]
+    except Exception as e:
+        print("Error uploading media to twitter")
+        print(e)
+    try:
+        if len(media_ids) == 0:
+            client_v2.create_tweet(text=tweet_text)
+        else:
+            client_v2.create_tweet(text=tweet_text, media_ids=media_ids)
+    except Exception as e:
+        print("couldn't send tweet")
+        print(e)
+
+    # except Exception as e:
+    #     print("tweet failed")
+    #     print(e)
+
+class Command(BaseCommand):
+
+    help = 'Post media to all socials'
+
+
+    def add_arguments(self, parser):
+        # Positional arguments
+        parser.add_argument("text", nargs="+", type=str)
+
+        # Named (optional) arguments
+        parser.add_argument(
+            "--m",
+            help="add filepath to uploadable media",
+        )
+
+
+    def handle(self, *args, **kwargs):
+
+        tweet_body = kwargs["text"][0]
+        filepath_to_media = None
+        if kwargs['m']:
+            filepath_to_media = kwargs['m']
+        tweet(tweet_body, filepath_to_media)
+        
